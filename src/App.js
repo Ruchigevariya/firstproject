f8f644af904478fb5b2be030e9aa17447ab8eca0
Ruchigevariya <gevariyaruchita@gmail.com>
2022-05-27 15:46:31 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Demo from './components/Demo';
 
 function App() {
   return (
-    <div>
-      <Demo/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React at js
+        </a>
+      </header>
     </div>
   );
 }
