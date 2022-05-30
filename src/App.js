e6ca678bd17b18d008bf5d700489ee8f17325147
Ruchigevariya <gevariyaruchita@gmail.com>
2022-05-30 21:12:41 +0530

    data

--- src/App.js
+++ src/App.js
@@ -58,30 +58,70 @@ function App() {
   //   }
   // ]
 
-  // person.map((p) => {console.log(p.name,p.age);
+  // person.map((p) => {
+  //   console.log(p.name,p.age);
   //   p.course.map((p1) => console.log(p1));
   // });
 
-  const myObj = {
-    name: "John",
-    age: 30,
-    cars: {
-        car1: "Ford",
-        car2: "BMW",
-        car3: "Fiat"
+  // const myObj = {
+  //   name: "John",
+  //   age: 30,
+  //   cars: {
+  //       car1: "Ford",
+  //       car2: "BMW",
+  //       car3: "Fiat"
+  //   }
+  // }
+
+  // for(let k in myObj){
+  //   if(k === 'cars'){
+  //     for(let j in myObj[k]){
+  //       console.log(myObj[k][j]);
+  //     }
+  //   }else{
+  //     console.log(myObj[k]);
+  //   }
+  // }
+
+  const data = {
+    personal_info: {
+        name: 'amit',
+        age: 25,
+        city: 'surat'
+    },
+    courses: ["C", "JavaScript", "React"],
+    branches: {
+        rwl: {
+            admission: 50,
+            vacant_seat: 10
+        },
+        rw2: {
+            admission: 30,
+            vacant_seat: 20
+        },
+        rw3: {
+            admission: 25,
+            vacant_seat: 25
+        },
+        rw4: {
+            admission: 40,
+            vacant_seat: 10
+        }
     }
   }
 
-  for(let k in myObj){
-    if(k === "cars"){
-      for(let j in myObj[k]){
-        console.log(myObj[k][j]);
-      }
+  for(let k in data){
+    if(k === 'personal_info'){
+      console.log(data[k].name,data[k].age,data[k].city);
+    }else if(k === 'courses'){
+      data[k].map((c) => console.log(c));
     }else{
-      console.log(myObj[k]);
+      for(let j in data[k]){
+        console.log(data[k][j].admission,data[k][j].vacant_seat);
+      }
     }
+    // console.log(data[k]);
   }
-
   return (
     <div>
       <Demo />
