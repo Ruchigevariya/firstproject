6b71a7ee7e726df2186d12c65a48feaa568f85bf
Ruchigevariya <gevariyaruchita@gmail.com>
2022-05-31 15:55:54 +0530

    changes....

--- src/App.js
+++ src/App.js
@@ -5,52 +5,121 @@ import Demo from './components/Demo';
 function App() {
 
     // Array:-
-    let arr1 = [10,20,30,40,50]
+    // let arr1 = [10,20,30,40,50]
 
-    // copy
-    let arr2 = [...arr1]
+    // // copy
+    // let arr2 = [...arr1]
 
-    console.log(arr2);
+    // console.log(arr2);
 
-    // Merging
-    let arr3 = [99,...arr1,100]
-    console.log(arr3);
+    // // Merging
+    // let arr3 = [99,...arr1,100]
+    // console.log(arr3);
 
-    // destructuring
-    let [a,b,c,d,e] =arr1
+    // // destructuring
+    // let [a,b,c,d,e] =arr1
     
-    console.log(c);
+    // console.log(c);
 
-    // object:-
+    // // object:-
 
-    let Obj1 ={
-      id:101,
-      name:'Ruchi'
-    };
+    // let Obj1 ={
+    //   id:101,
+    //   name:'Ruchi'
+    // };
   
-    //copy 
-    let Obj2 ={...Obj1}
+    // //copy 
+    // let Obj2 ={...Obj1}
 
-    console.log(Obj2);
+    // console.log(Obj2);
 
-    // Merging
-    let Obj3 ={
-      id:102,
-      name:'Richa',
-      location:'Surat',
-      ...Obj1
-    };
+    // // Merging
+    // let Obj3 ={
+    //   id:102,
+    //   name:'Richa',
+    //   location:'Surat',
+    //   ...Obj1
+    // };
 
-    console.log(Obj3);
+    // console.log(Obj3);
 
-    // destructuring
-    let {id,name,location} = Obj1
+    // // destructuring
+    // let {id,name,location} = Obj1
 
-    console.log(name);
+    // console.log(name);
+
+    let data =
+    [
+       {
+         id: 101,
+         name: 'Abacavir',
+         quantity: 25,
+         price: 150,
+         expiry: 2022,
+         status: true
+       },
+       {
+         id: 102,
+         name: 'Eltrombopag',
+         quantity: 90,
+         price: 550,
+         expiry: 2021,
+         status: true
+       },
+       {
+         id: 103,
+         name: 'Meloxicam',
+         quantity: 85,
+         price: 450,
+         expiry: 2025,
+         status: false
+       },
+       {
+         id: 104,
+         name: 'Allopurinol',
+         quantity: 50,
+         price: 600,
+         expiry: 2023,
+         status: true
+       },
+       {
+         id: 105,
+         name: 'Phenytoin',
+         quantity: 63,
+         price: 250,
+         expiry: 2021,
+         status: false
+       }
+     ];
   return (
     <div>
-      <Demo />
+      <table border='1'>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Quantity</th>
+        <th>Price</th>
+        <th>Expiry</th>
+        <th>Status</th>
+     
+      {
+    
+          data.map((d) => {
+            return(
+              <tr>
+                <td>{d.id}</td>
+                <td>{d.name}</td>
+                <td>{d.quantity}</td>
+                <td>{d.price}</td>
+                <td>{d.expiry}</td>
+                <td>{d.status.toString()}</td>
+            </tr>
+            )
+          })
+        
+        }
+        </table> 
     </div>
+  
   );
 }
 
