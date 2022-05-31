edbe4edc81b4e7a16bbddcb1f886306b3cbd1ab1
Ruchigevariya <gevariyaruchita@gmail.com>
2022-05-31 15:11:14 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,140 +4,24 @@ import Demo from './components/Demo';
 
 function App() {
 
-  // const person = {
-  //   name: "Amit",
-  //   age: 25
-  // }
+    // Array:-
+    // let arr1 = [10,20,30,40,50]
 
-  // console.log(person.name,person.age);
+    // // copy
+    // let arr2 = [...arr1]
 
-  // const person = [
-  //   {
-  //       name: "Amit",
-  //       age: 25,
-  //   },
-  //   {
-  //       name: "Piyush",
-  //       age: 40,
-  //   },
+    // console.log(arr2);
 
-  // ];
+    // // Merging
+    // let arr3 = [99,...arr1,100]
+    // console.log(arr3);
 
-  // person.map((p) => console.log(p.name,p.age));
+    // // destrcuting
+    // let [a,b,c,d,e] =arr1
+    
+    // console.log(c);
 
-
-  // const person = {
-  //   name: "Amit",
-  //   age: 25,
-  //   course: [
-  //       "C",
-  //       "HTML"
-  //   ]
-  // }
-
-  // console.log(person.name,person.age);
-
-  // person.course.map((p) => console.log(p));
-
-  // const person = [
-  //   {
-  //       name: "Amit",
-  //       age: 25,
-  //       course: [
-  //           "C",
-  //           "HTML"
-  //       ]
-  //   },
-  //   {
-  //       name: "Ajay",
-  //       age: 40,
-  //       course: [
-  //           "Java",
-  //           "JavaScript"
-  //       ]
-  //   }
-  // ]
-
-  // person.map((p) => {
-  //   console.log(p.name,p.age);
-  //   p.course.map((p1) => console.log(p1));
-  // });
-
-  // const myObj = {
-  //   name: "John",
-  //   age: 30,
-  //   cars: {
-  //       car1: "Ford",
-  //       car2: "BMW",
-  //       car3: "Fiat"
-  //   }
-  // }
-
-  // for(let k in myObj){
-  //   if(k === 'cars'){
-  //     for(let j in myObj[k]){
-  //       console.log(myObj[k][j]);
-  //     }
-  //   }else{
-  //     console.log(myObj[k]);
-  //   }
-  // }
-
-  // const data = {
-  //   personal_info: {
-  //       name: 'amit',
-  //       age: 25,
-  //       city: 'surat'
-  //   },
-  //   courses: ["C", "JavaScript", "React"],
-  //   branches: {
-  //       rwl: {
-  //           admission: 50,
-  //           vacant_seat: 10
-  //       },
-  //       rw2: {
-  //           admission: 30,
-  //           vacant_seat: 20
-  //       },
-  //       rw3: {
-  //           admission: 25,
-  //           vacant_seat: 25
-  //       },
-  //       rw4: {
-  //           admission: 40,
-  //           vacant_seat: 10
-  //       }
-  //   }
-  // }
-
-  // for(let k in data){
-  //   if(k === 'personal_info'){
-  //     console.log(data[k].name,data[k].age,data[k].city);
-  //   }else if(k === 'courses'){
-  //     data[k].map((c) => console.log(c));
-  //   }else{
-  //     for(let j in data[k]){
-  //       console.log(data[k][j].admission,data[k][j].vacant_seat);
-  //     }
-  //   }
-  //   // console.log(data[k]);
-  // }
-
-    // Array
-    let arr1 = [10,20,30,40,50]
-
-    // copy
-    let arr2 = [...arr1]
-
-    console.log(arr2);
-
-    // Merging
-    let arr3 = [99,...arr1,100]
-    console.log(arr3);
-
-    // destrcuting
-    let  [a,b,c,d,e]  
-
+    // object:-
   return (
     <div>
       <Demo />
