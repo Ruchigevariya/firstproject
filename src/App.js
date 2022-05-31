c564034bb97deb377b4fcf6330be4de2c81d9b0e
Ruchigevariya <gevariyaruchita@gmail.com>
2022-05-31 15:27:03 +0530

    changes..

--- src/App.js
+++ src/App.js
@@ -16,12 +16,37 @@ function App() {
     // let arr3 = [99,...arr1,100]
     // console.log(arr3);
 
-    // // destrcuting
+    // // destructuring
     // let [a,b,c,d,e] =arr1
     
     // console.log(c);
 
     // object:-
+
+    let Obj1 ={
+      id:101,
+      name:'Ruchi'
+    };
+  
+    //copy 
+    let Obj2 ={...Obj1}
+
+    console.log(Obj2);
+
+    // Merging
+    let Obj3 ={
+      id:102,
+      name:'Richa',
+      location:'Surat',
+      ...Obj1
+    };
+
+    console.log(Obj3);
+
+    // destructuring
+    let {id,name,location} = Obj1
+
+    console.log(name);
   return (
     <div>
       <Demo />
