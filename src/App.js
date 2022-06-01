8bc18a7e2620248851f8fe3827088f8901b091bf
Ruchigevariya <gevariyaruchita@gmail.com>
2022-06-01 10:58:54 +0530

    changes.

--- src/App.js
+++ src/App.js
@@ -4,105 +4,95 @@ import Demo from './components/Demo';
 
 function App() {
 
-    // Array:-
-    // let arr1 = [10,20,30,40,50]
+  // Array:-
+  // let arr1 = [10,20,30,40,50]
 
-    // // copy
-    // let arr2 = [...arr1]
+  // // copy
+  // let arr2 = [...arr1]
 
-    // console.log(arr2);
+  // console.log(arr2);
 
-    // // Merging
-    // let arr3 = [99,...arr1,100]
-    // console.log(arr3);
+  // // Merging
+  // let arr3 = [99,...arr1,100]
+  // console.log(arr3);
 
-    // // destructuring
-    // let [a,b,c,d,e] = arr1
-    
-    // console.log(c);
+  // // destructuring
+  // let [a,b,c,d,e] = arr1
 
-    // // object:-
+  // console.log(c);
 
-    // let Obj1 ={
-    //   id:101,
-    //   name:'Ruchi'
-    // };
-  
-    // //copy 
-    // let Obj2 ={...Obj1}
+  // // object:-
 
-    // console.log(Obj2);
+  // let Obj1 ={
+  //   id:101,
+  //   name:'Ruchi'
+  // };
 
-    // // Merging
-    // let Obj3 ={
-    //   id:102,
-    //   name:'Richa',
-    //   location:'Surat',
-    //   ...Obj1
-    // };
+  // //copy 
+  // let Obj2 ={...Obj1}
 
-    // console.log(Obj3);
+  // console.log(Obj2);
 
-    // // destructuring
-    // let {id,name,location} = Obj1
+  // // Merging
+  // let Obj3 ={
+  //   id:102,
+  //   name:'Richa',
+  //   location:'Surat',
+  //   ...Obj1
+  // };
 
-    // console.log(name);
+  // console.log(Obj3);
 
-    let data =
+  // // destructuring
+  // let {id,name,location} = Obj1
+
+  // console.log(name);
+
+  let data =
     [
-       {
-         id: 101,
-         name: 'Abacavir',
-         quantity: 25,
-         price: 150,
-         expiry: 2022,
-         status: true
-       },
-       {
-         id: 102,
-         name: 'Eltrombopag',
-         quantity: 90,
-         price: 550,
-         expiry: 2021,
-         status: true
-       },
-       {
-         id: 103,
-         name: 'Meloxicam',
-         quantity: 85,
-         price: 450,
-         expiry: 2025,
-         status: false
-       },
-       {
-         id: 104,
-         name: 'Allopurinol',
-         quantity: 50,
-         price: 600,
-         expiry: 2023,
-         status: true
-       },
-       {
-         id: 105,
-         name: 'Phenytoin',
-         quantity: 63,
-         price: 250,
-         expiry: 2021,
-         status: false
-       }
-     ];
+      {
+        id: 101,
+        name: 'Abacavir',
+        quantity: 25,
+        price: 150,
+        expiry: 2022,
+        status: true
+      },
+      {
+        id: 102,
+        name: 'Eltrombopag',
+        quantity: 90,
+        price: 550,
+        expiry: 2021,
+        status: true
+      },
+      {
+        id: 103,
+        name: 'Meloxicam',
+        quantity: 85,
+        price: 450,
+        expiry: 2025,
+        status: false
+      },
+      {
+        id: 104,
+        name: 'Allopurinol',
+        quantity: 50,
+        price: 600,
+        expiry: 2023,
+        status: true
+      },
+      {
+        id: 105,
+        name: 'Phenytoin',
+        quantity: 63,
+        price: 250,
+        expiry: 2021,
+        status: false
+      }
+    ];
 
-    // destructuring
-
-    // data.map((d) => {
-    //   console.log(d)
-      
-    // });
-    // let {id,name,quantity,price,expiry,status} = data
-
-    // console.log(id);
-
-    let employeedata =
+  let employeedata =
     [
       {
         name: "amit",
@@ -140,8 +130,9 @@ function App() {
         status: true
       },
     ]
+
   return (
-    <div>
+    <div style={{margin:'5px'}}>
       <table border='1'>
         <th>Id</th>
         <th>Name</th>
@@ -149,24 +140,35 @@ function App() {
         <th>Price</th>
         <th>Expiry</th>
         <th>Status</th>
-     
-      {
-    
+
+        {
+
           data.map((d) => {
-            return(
+            let {id,name,quantity,price,expiry,status} = d
+
+            return (
+
               <tr>
-                <td>{d.id}</td>
+
+                <td>{id}</td>
+                <td>{name}</td>
+                <td>{quantity}</td>
+                <td>{price}</td>
+                <td>{expiry}</td>
+                <td>{status.toString()}</td>
+              
+                {/* <td>{d.id}</td>
                 <td>{d.name}</td>
                 <td>{d.quantity}</td>
                 <td>{d.price}</td>
                 <td>{d.expiry}</td>
-                <td>{d.status.toString()}</td>
+                <td>{d.status.toString()}</td> */}
               </tr>
             )
           })
-        
-      }
-      </table> 
+
+        }
+      </table>
 
       <table border='1'>
         <th>Name</th>
@@ -174,29 +176,38 @@ function App() {
         <th>Salary</th>
         <th>Bonus</th>
         <th>Status</th>
-     
-      {
+
+        {
 
           employeedata.map((e) => {
-            return(
+            let {name,age,salary,bonus,status} = e
+            return (
+              
               <tr>
-                <td>{e.name}</td>
+
+                <td>{name}</td>
+                <td>{age}</td>
+                <td>{salary}</td>
+                <td>{bonus}</td>
+                <td>{status.toString()}</td>
+
+                {/* <td>{e.name}</td>
                 <td>{e.age}</td>
                 <td>{e.salary}</td>
                 <td>{e.bonus}</td>
-                <td>{e.status.toString()}</td>
+                <td>{e.status.toString()}</td> */}
               </tr>
             )
           })
-        
-      }
-      </table> 
+
+        }
+      </table>
     </div>
-  
+
   );
 
-     
-  
+
+
 }
 
 export default App;
