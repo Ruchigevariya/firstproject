ac261630a6a760be67c3d8b9658091b7aea6fece
Ruchigevariya <gevariyaruchita@gmail.com>
2022-06-03 10:48:19 +0530

    CHANGES

--- src/App.js
+++ src/App.js
@@ -63,25 +63,25 @@ function App() {
   //   p.course.map((p1) => console.log(p1));
   // });
 
-  // const myObj = {
-  //   name: "John",
-  //   age: 30,
-  //   cars: {
-  //       car1: "Ford",
-  //       car2: "BMW",
-  //       car3: "Fiat"
-  //   }
-  // }
+  const myObj = {
+    name: "John",
+    age: 30,
+    cars: {
+        car1: "Ford",
+        car2: "BMW",
+        car3: "Fiat"
+    }
+  }
 
-  // for(let k in myObj){
-  //   if(k === 'cars'){
-  //     for(let j in myObj[k]){
-  //       console.log(myObj[k][j]);
-  //     }
-  //   }else{
-  //     console.log(myObj[k]);
-  //   }
-  // }
+  for(let k in myObj){
+    if(k === 'cars'){
+      for(let j in myObj[k]){
+        console.log(myObj[k][j]);
+      }
+    }else{
+      console.log(myObj[k]);
+    }
+  }
 
   const data = {
     personal_info: {
