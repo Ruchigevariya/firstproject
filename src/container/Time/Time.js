c9fe7ae70af2235025a80aa3b7d1a6227038bdd3
Ruchigevariya <gevariyaruchita@gmail.com>
2022-06-06 15:47:47 +0530

    intial changes.

--- src/container/Time/Time.js
+++ src/container/Time/Time.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 class Time extends Component {
     constructor(props) {
         super(props);
+       
+        // 1 Used to set Initial value to the component.
         
         this.state = {
             time: new Date()
         }
-
     }
 
     tick = () =>{
@@ -17,18 +18,24 @@ class Time extends Component {
     }
     
     componentDidMount = () => {
+        // 3 Used to request data from server.
+
         this.setI = setInterval(() => this.tick(),1000);
     }
 
     componentDidUpdate = (prevProps ,prevState) => {
+        // 4 Called whenever particular state/props Updated.
+
         if(prevState !== this.setI)
         console.log("componentDidUpdate");
     }
 
     componentWillUnmount = () => {
+        // 5 Called whenever we move to another component (used to release the resources)
         clearInterval(this.setI);
     }
 
+    // 2 Called whenever state value changed
     render() {
         return (
             <div>
